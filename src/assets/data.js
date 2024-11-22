abcb4a3716aeec11dbf40789414f8f3e3267c6c5
surender18 <surenderempire68@gmail.com>
2024-11-23 00:33:04 +0530

    final commit

---
+++ src/assets/data.js
@@ -0,0 +1,42 @@
+export const data= [
+    {
+      question: "How can you optimize a React app?",
+      option1: "Avoid useEffect",
+      option2:"Use keys",
+      option3:"Conditional rendering",
+        option4:"Inline styles",
+        ans: 2
+    },
+    {
+      question: " Difference between relative and absolute positioning?",
+      option1: "Containing block",
+      option2:"Viewport",
+      option3:"Normal position ",
+        option4:"Fixed position",
+        ans: 3
+    },
+    {
+      question: " In CSS, what is the correct syntax for selecting elements with the class button?",
+      option1: "button",
+      option2:"#button",
+      option3:".button",
+        option4:"*button",
+        ans: 3
+    },
+    {
+      question: " Purpose of React.memo()",
+      option1: "Memoize hooks",
+      option2:"Prevent re-renders",
+      option3:"Replace state",
+        option4:"Add lifecycle",
+        ans: 2
+    },
+    {
+      question: " Purpose of debounce function?",
+      option1: "Delay execution",
+      option2:"Immediate execution",
+      option3:"Multiple executions",
+        option4:"Group functions",
+        ans: 1
+    },
+]
